4f0567c21aa66c196c3217452ba80620bcaad1aa
gcliau100 <gabliau100@gmail.com>
2024-06-20 21:21:43 -1000

    Wrote some stuff in AboutMe

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './About.css';
+
 
 const About = () => {
   return (
-    <div>
+    <div className="about-content">
       <h1>About Me</h1>
-      <p>This is the about page.</p>
+      <div className="whalephisher-content">
+        <h2>Why Whalephisher?</h2>
+        <p>No it is not because I like fishing/hunting whales or have a huge love of whales (although I do think whales are cool). I chose the handle Whalephisher because it is a name inspired by the intricate art of whale phishing in cybersecurity. Just as whale phishing targets high-profile individuals with precision and finesse, I approach hacking with a blend of technical expertise and strategic insight. My journey in cybersecurity has been shaped by a passion for understanding complex systems and a dedication to mastering the art of social engineering. With each challenge, I aim not only to navigate the digital landscape but also to innovate creatively, always pushing the boundaries of what's possible.</p>
+      </div>
     </div>
   );
 };
